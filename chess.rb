e15c60760c92c23fe90ea89ae06f5d2b2c511483
Marie <marie.hepfer@gmail.com>
2014-10-14 18:08:39 -0700

    end of day one.  debugging valid_moves

--- chess.rb
+++ chess.rb
@@ -1,15 +1,130 @@
-class Chess_board
+require 'colorize'
+
+class Chessboard
+  attr_accessor :board
+  
   def initialize
     @board = Array.new(8){Array.new(8) {nil}}
+    populate_board
   end
   
-  def position
+  def populate_board
+    self[[0, 0]] = Rook.new(self, [0, 0], "b")
+    self[[0, 7]] = Rook.new(self, [0, 7], "b")
+    self[[7, 0]] = Rook.new(self, [7, 0], "w")
+    self[[7, 7]] = Rook.new(self, [7, 7], "w")
+
+    self[[0, 1]] = Knight.new(self, [0, 1], "b")
+    self[[0, 6]] = Knight.new(self, [0, 6], "b")
+    self[[7, 1]] = Knight.new(self, [7, 1], "w")
+    self[[7, 6]] = Knight.new(self, [7, 6], "w")
+
+    self[[0, 2]] = Bishop.new(self, [0, 2], "b")
+    self[[0, 5]] = Bishop.new(self, [0, 5], "b")
+    self[[7, 2]] = Bishop.new(self, [7, 2], "w")
+    self[[7, 5]] = Bishop.new(self, [7, 5], "w")
+
+    self[[0, 3]] = Queen.new(self, [0, 3], "b")
+    self[[7, 3]] = Queen.new(self, [7, 3], "w")
+
+    self[[0, 4]] = King.new(self, [0, 4], "b")
+    self[[7, 4]] = King.new(self, [7, 4], "w")
+    
+    8.times do |y|
+      self[[1, y]] = Pawn.new(self, [1, y], "b")
+      self[[6, y]] = Pawn.new(self, [6, y], "w")
+    end
+    
+    def display_board
+      display_string = ""
+      alternate_color = true
+      @board.each_with_index do |rows, row_index|
+        rows.each_with_index do |space, col_index|
+          if alternate_color
+            display_string << " ".on_black if space.nil?
+            display_string << space.to_s.colorize(:white).on_black
+            display_string << "  ".on_black
+          else
+            display_string << " ".on_red if space.nil?
+            display_string << space.to_s.colorize(:white).on_red
+            display_string << "  ".on_red
+          end
+          alternate_color = !alternate_color
+          
+        end
+        display_string << "\n"
+        alternate_color = !alternate_color
+      end
+      puts display_string
+    end
     
   end
   
   def [](position)
     x,y = position
-    self[x][y]
+    @board[x][y]
+  end
+  
+  def []=(position, piece)
+    x,y = position
+    @board[x][y] = piece
+  end
+  
+  def in_check?(color)
+    king_position = find_king(color)
+    all_enemy_pieces(color).any? {|piece| piece.moves.include? king_position}
+  end
+  
+  def all_enemy_pieces(color)
+    result = []
+    @board.each do |row|
+      row.each do |piece|
+        if piece != nil && piece.color != color
+          result << piece 
+        end
+      end
+    end
+    result
+  end
+  
+  def find_king(color)
+    @board.each_with_index do |row, x|
+      row.each_with_index do |piece, y|
+        if piece.class == King && piece.color == color
+          return piece.position
+        end
+      end
+    end
+  end
+  
+  def move(start_pos, end_pos)
+    raise StandardError.new("No piece there!") if self[start_pos].nil?
+    piece_to_move = self[start_pos]
+    unless piece_to_move.moves.include?(end_pos)
+      raise StandardError.new("Can't move there!")   
+    end
+    self[start_pos] = nil
+    piece_to_move.position = end_pos
+    unless self[end_pos].nil?
+      captured_piece = self[end_pos]
+      captured_piece.position = nil
+    end
+    self[end_pos] = piece_to_move
+    
+  end
+  
+  def dup
+    board_dup = Chessboard.new
+    board_dup.board = Array.new(8){Array.new(8) {nil}}
+    self.board.each_with_index do |row, row_index|
+      row.each_with_index do |piece, col_index|
+        unless piece.nil?
+          new_piece = piece.class.new(board_dup, [row_index,col_index], piece.color)
+          board_dup[[row_index,col_index]] = new_piece
+        end
+      end
+    end
+    board_dup
   end
   
 end
@@ -17,8 +132,10 @@ end
 class Piece
   DIAGONALS = [[1,1], [-1, 1], [-1, -1], [1, -1]]
   ORTHOGONALS = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-
-
+  
+  attr_reader :color
+  attr_accessor :position, :board
+  
   def initialize(board, position, color)
     @board, @position, @color = board, position, color
   end
@@ -30,7 +147,23 @@ class Piece
     
     self.color != board[position].color
   end
+  
+  def valid_moves
+    moves = self.moves
+    moves.reject do |move|
+      self.move_into_check?(move)
+    end
+  end
 
+  def move_into_check?(pos)
+    board_dup = self.board.dup
+    board_dup[self.position] = nil
+    board_dup[pos] = self
+    board_dup.in_check?(self.color)
+  end
+  
+
+  
   private
   
   def update_position(position, offset)
@@ -50,6 +183,7 @@ class Piece
   def on_board?(position)
     position.first.between?(0, 7) && position.last.between?(0, 7)
   end
+
 end
 
 class SlidingPiece < Piece
@@ -73,17 +207,29 @@ class Rook < SlidingPiece
   def move_dirs
     ORTHOGONALS
   end
+  
+  def to_s
+    @color == "w" ? "\u2656" : "\u265C"
+  end
 end
 
 class Bishop < SlidingPiece
   def move_dirs
     DIAGONALS
   end 
+  
+  def to_s
+    @color == "w" ? "\u2657" : "\u265D"
+  end
 end
 
 class Queen < SlidingPiece
   def move_dirs
     DIAGONALS + ORTHOGONALS
+  end
+  
+  def to_s
+    @color == "w" ? "\u2655" : "\u265B"
   end
 end
 
@@ -91,7 +237,6 @@ class SteppingPiece < Piece
   
   def moves
     move_array = []
-    p move_dirs
     move_dirs.each do |offset|
       position = update_position(@position, offset)
       unless invalid?(position)
@@ -107,12 +252,20 @@ class King < SteppingPiece
   def move_dirs
     DIAGONALS + ORTHOGONALS
   end
+  
+  def to_s
+    @color == "w" ? "\u2654" : "\u265A"
+  end
 end
 
 class Knight < SteppingPiece
   
   def move_dirs
     [[2,1],[2,-1],[1,2],[1,-2],[-2,1],[-2,-1],[-1,2],[-1,-2]]
+  end
+  
+  def to_s
+    @color == "w" ? "\u2658" : "\u265E"
   end
   
 end
@@ -148,6 +301,10 @@ class Pawn < Piece
     end
     
     offset_array.map{|offset| update_position(@position, offset)}
+  end
+  
+  def to_s
+    @color == "w" ? "\u2659" : "\u265F"
   end
   
 end
